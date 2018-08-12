43ff97a09771ed8499883be4e1abc34742bce2c3
Jiang Han <jh.wk.dev@gmail.com>
2018-08-12 14:05:26 +0900

    schema更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180812041908) do
+ActiveRecord::Schema.define(version: 20180812045437) do
+
+  create_table "cards", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "company_id"
+    t.string "department"
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_cards_on_company_id"
+    t.index ["user_id"], name: "index_cards_on_user_id"
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
